3ba768410dbe62a5a3e85d362d7faf5066bcf939
riarana1 <riarana1@yahoo.com>
2024-06-29 16:41:50 -0500

    Amazona eshop init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <header>eShop Amazona</header>
+      <main></main>
+      <footer>All rights reserved</footer>
+    </div>
+  )
+}
+
+export default App
